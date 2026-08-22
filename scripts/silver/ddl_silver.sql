0e81e3bb375f26a4006ea1d8547d66cd0edf9e85
Jugaru Paul <paul.jugaru@student.unitbv.ro>
2026-08-22 16:24:07 +0300

    Update ddl_silver.sql
    Updated data types for sales order dates in the crm_prd_info table.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -28,6 +28,7 @@ IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
 CREATE TABLE silver.crm_prd_info
 (
 	prd_id INT,
+	cat_id NVARCHAR(50),
 	prd_key NVARCHAR(50),
 	prd_nm NVARCHAR(50),
 	prd_cost INT,
@@ -44,9 +45,9 @@ CREATE TABLE silver.crm_sales_details
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
     sls_cust_id INT,
-    sls_order_dt INT,
-    sls_ship_dt INT,
-    sls_due_dt INT,
+    sls_order_dt DATE,
+    sls_ship_dt DATE,
+    sls_due_dt DATE,
     sls_sales INT,
     sls_quantity INT,
     sls_price INT,
